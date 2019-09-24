42d4a0b364d402b25795f3a0a98cee30eaf6292f
Ryan Jansen <ryanj@openquire.com>
2019-09-24 15:18:29 -0400

    support for custom expiration times on keys; allow client to create key if it doesn't exist

--- lib/qmac/key_providers/redis.rb
+++ lib/qmac/key_providers/redis.rb
@@ -5,11 +5,14 @@ module Qmac
 
       class Configurator
 
+        attr_reader :expiration_interval, :redis_url
+
         def initialize
+          self.expiration_interval = 30.minutes
         end
 
-        def redis_url
-          @redis_url
+        def expiration_interval=(value)
+          @expiration_interval = value
         end
 
         def redis_url=(value)
@@ -33,14 +36,39 @@ module Qmac
           end
         end
 
-        def private_key_for(key)
+        # supported options
+        # create: true (optionally create the private key if it doesn't already exist)
+        def private_key_for(key_space, options = {})
           self._verify_configured
           @connection.with do |redis|
-            redis.get(key)
+            if options.fetch(:create, false)
+              while true
+                result = self._update_private_key_safe(redis, key_space)
+                break result unless result.nil?
+              end
+            else
+              redis.get(key_space)
+            end
           end
         end
 
         protected
+
+        def _update_private_key_safe(redis, key_space)
+          redis.watch(key_space) do
+            result = redis.get(key_space)
+            if result.nil?
+              key = SecureRandom.hex(16)
+              result = redis.multi do |multi|
+                redis.set(key_space, key, ex: @configurator.expiration_interval)
+              end
+              result.nil? ? nil : key
+            else
+              redis.unwatch
+              result
+            end
+          end
+        end
 
         def _verify_configured
           raise 'No configuration provided' unless @configurator
